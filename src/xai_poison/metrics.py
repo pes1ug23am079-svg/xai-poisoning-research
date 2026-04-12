49a99f4240930e2a4a1a3eef59dd899a757f83c6
rhrishi-99 <rgrhrishi@gmail.com>
2026-04-12 18:07:39 +0530

    fixes

--- src/xai_poison/metrics.py
+++ src/xai_poison/metrics.py
@@ -6,7 +6,6 @@ import pandas as pd
 import seaborn as sns
 from scipy.stats import spearmanr
 
-
 # ---------------------------------------------------------------------------
 # Core metrics
 # ---------------------------------------------------------------------------
